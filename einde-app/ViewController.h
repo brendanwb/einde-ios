7a13d981b0e60490104a8cd4002b28b2ef174ff5
Brendan Buckingham <brendan.buckingham@gmail.com>
2016-03-13 23:10:55 -0500

    Initial Commit

---
+++ einde-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  einde-app
+//
+//  Created by Brendan Buckingham on 3/13/16.
+//  Copyright © 2016 Brendan Buckingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ einde-app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  einde-app
+//
+//  Created by Brendan Buckingham on 3/13/16.
+//  Copyright © 2016 Brendan Buckingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
